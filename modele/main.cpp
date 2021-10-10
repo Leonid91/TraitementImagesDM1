f7fe11b61b98a869b12caab45e9d139846fe143d
NS <aines.bouziane@yahoo.fr>
2021-10-11 01:05:55 +0200

    try catch pour éviter que ça plante

--- modele/main.cpp
+++ modele/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char** argv){
 	//Mat imageOut = Mat::zeros(Size(imageIn.cols, imageIn.rows), fragIn.depth()); //Ne marche pas car en nuances de gris
 	Mat imageOut = imageIn.clone(); //Copie de l'image originale
 	imageOut = Scalar(255,255,255); //On la remplie de blanc
-	//On créer un channel alpha pour l'image de sortie pour gérer la transparence
+	//On crée un channel alpha pour l'image de sortie pour gérer la transparence
 	Mat newImageOut(imageOut.size(), CV_MAKE_TYPE(imageOut.depth(), 4));
 	int from_to[] = { 0,0, 1,1, 2,2, -1,3 };
 	mixChannels(&imageOut,1,&newImageOut,1,from_to,4);
@@ -66,16 +66,22 @@ int main(int argc, char** argv){
 	//}
 
 	//Traitement
-	// for(int i=0; i<listOfRotations.size();i++){
-	for(int i=0; i<12;i++){ //On test juste avec quelques fragments
+	 for(int i=0; i<listOfRotations.size();i++){
+	//for(int i=0; i<12;i++){ //On test juste avec quelques fragments
 		if(listOfPositionsX[i] != -1){
 			Mat frag = imread("../../frag_eroded/frag_eroded_" + std::to_string(i) + ext, IMREAD_UNCHANGED);
-			imwrite("../../beforeRotation.png", frag);
+			Mat fragGray = imread("../../frag_eroded/frag_eroded_" + std::to_string(i) + ext, IMREAD_GRAYSCALE);
+			//imwrite("../../beforeRotation.png", frag);
+
+			Mat thresholded;
+			threshold(fragGray, thresholded, 0, 255, THRESH_OTSU);
+			imwrite("../../otsu.png", thresholded);
+			Rect bbox = boundingRect(thresholded);
 
 			Mat fragRotated;
 			Point2f centre((frag.cols-1)/2.0, (frag.rows-1)/2.0);
 			Mat rotationMatrix = getRotationMatrix2D(centre, listOfRotations[i], 1.0); //Matrice de rotation
-			std::cout << "Image " << i << " rotation : " << listOfRotations[i] << std::endl;
+			//std::cout << "Image " << i << " rotation : " << listOfRotations[i] << std::endl;
 
 			//Pour éviter de rogner pendant la rotation
 			//Rect2f bbox = RotatedRect(Point2f(), frag.size(), listOfRotations[i]).boundingRect2f();
@@ -84,30 +90,41 @@ int main(int argc, char** argv){
 			//warpAffine(frag, fragRotated, rotationMatrix, bbox.size());
 			//imwrite("../../afterRotationBox.png", fragRotated);
 
-			warpAffine(frag, fragRotated, rotationMatrix, frag.size());
-			imwrite("../../afterRotation.png", fragRotated);
-
-			fragRotated.copyTo(newImageOut(Rect(listOfPositionsX[i], listOfPositionsY[i], fragRotated.cols, fragRotated.rows)));
-			//addWeighted(newImageOut(Rect(listOfPositionsX[i], listOfPositionsY[i], fragRotated.cols, fragRotated.rows)), 0.8, fragRotated, 0.2, 0.0, newImageOut);
-			std::cout << "Fragment " << std::to_string(i) << " copié." << std::endl;
+			warpAffine(frag, fragRotated, rotationMatrix, frag.size()); //Application de la matrice de rotation
+			//imwrite("../../afterRotation.png", fragRotated);
 
 			//On retire le fond noir après chaque copie
-			Mat input_bgra;
-			cvtColor(newImageOut, input_bgra, COLOR_BGR2BGRA);
+			Mat frag_bgra;
+			cvtColor(fragRotated, frag_bgra, COLOR_BGR2BGRA);
 
-			for (int y = 0; y < input_bgra.rows; ++y)
-			for (int x = 0; x < input_bgra.cols; ++x)
+			for (int y = 0; y < frag_bgra.rows; ++y)
 			{
-				Vec4b & pixel = input_bgra.at<cv::Vec4b>(y, x);
-				if (pixel[0] == 0 && pixel[1] == 0 && pixel[2] == 0)
+				for (int x = 0; x < frag_bgra.cols; ++x)
 				{
-					pixel[3] = 1;
+					Vec4b & pixel = frag_bgra.at<cv::Vec4b>(y, x);
+					if (pixel[0] == 0 && pixel[1] == 0 && pixel[2] == 0)
+					{
+						pixel[3] = 0;
+					} else {
+						pixel[3] = 255;
+					}
 				}
 			}
 
-			//cv::imwrite("../../transparentWhite.png", input_bgra);
 
-			//Comparer les channels des 2 imgs*
+			imwrite("../../fragAlpha.png", frag_bgra);
+
+			try{
+				frag_bgra.copyTo(newImageOut(Rect(listOfPositionsX[i], listOfPositionsY[i], frag_bgra.cols, frag_bgra.rows)));
+			}
+			catch (Exception e){
+
+			}
+
+			//addWeighted(newImageOut(Rect(listOfPositionsX[i], listOfPositionsY[i], fragRotated.cols, fragRotated.rows)), 0.8, fragRotated, 0.2, 0.0, newImageOut);
+			std::cout << "Fragment " << std::to_string(i) << " copié." << std::endl;
+
+			//Comparer les channels des imgs*
 			//std::cout<< frag.channels() << std::endl;
 			//std::cout<< fragRotated.channels() << std::endl;
 			//std::cout << newImageOut.channels() << std::endl;
